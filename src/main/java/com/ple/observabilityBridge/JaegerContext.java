164b8b4dc98be6b6b6c46f85df0f58738633aafc
jared <jared@pop-os.localdomain>
2022-06-09 15:30:20 -0700

    minor update

--- src/main/java/com/ple/observabilityBridge/JaegerContext.java
+++ src/main/java/com/ple/observabilityBridge/JaegerContext.java
@@ -1,10 +1,16 @@
 package com.ple.observabilityBridge;
 
-import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.context.Context;
 
+//TODO: Mirror this after ArrayBasedContext except instead of normal maps, make it Immutable Maps.
 public class JaegerContext implements HandlerContext {
+  public final Context context;
+
+  public JaegerContext(Context context) {
+    this.context = context;
+  }
+
   public Context get() {
-    return null;
+    return context;
   }
 }
--- src/main/java/com/ple/observabilityBridge/JaegerHandler.java
+++ src/main/java/com/ple/observabilityBridge/JaegerHandler.java
@@ -20,7 +20,12 @@ import io.prometheus.client.Counter;
 public class JaegerHandler implements RecordingHandler<JaegerContext> {
   public static JaegerHandler only = new JaegerHandler("http://localhost:4137");
   public final Tracer tracer;
+  public final Tracer globalTracer;
 
+  /**
+   *
+   * @param jaegerEndpoint
+   */
   private JaegerHandler(String jaegerEndpoint) {
 //    Resource serviceNameResource = Resource.create(Attributes.of(AttributeKey.stringKey("hello"), "otel-jaeger-example"));
 //    Resource resource = Resource.builder().put("hello", "otel-jaeger-example").build();
@@ -30,11 +35,22 @@ public class JaegerHandler implements RecordingHandler<JaegerContext> {
             .build()).build())
         .build();
 
+    // Jaeger cannot handle multi-plexing (reactive programming is a type of multi-plexing) multiplexing is just single
+    // thread doing multiple tasks at the same time. We need it to handle multi-plexing for it to work with vertx. Because
+    // vertx will do lots with one single thread and also do multiple threads at the same time.
     OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
         .setTracerProvider(sdkTracerProvider)
-        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+//        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+        .build();
+
+    //TODO: Figure out what is the use of global.
+    OpenTelemetry openTelemetryGlobal = OpenTelemetrySdk.builder()
+        .setTracerProvider(sdkTracerProvider)
+//        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
         .buildAndRegisterGlobal();
+
     tracer = openTelemetry.getTracer("instrumentation-library-name", "1.0.0");
+    globalTracer = openTelemetryGlobal.getTracer("instrumentation-library-name", "1.0.0");
   }
 
   public static JaegerHandler make(String jaegerEndpoint) {
@@ -43,6 +59,12 @@ public class JaegerHandler implements RecordingHandler<JaegerContext> {
 
   @Override
   public JaegerContext open(JaegerContext context, String group, IMap<String, String> dimensions) {
+    Span span = this.tracer.spanBuilder("Start my wonderful use case").setParent(context.get())
+        .startSpan();
+    span.addEvent("Event 0");
+    span.addEvent("Event 1");
+    span.end();
+/*
     Span child = tracer.spanBuilder("child span").setParent(context.get()).startSpan();
       // put the span into the current Context
     try {
@@ -53,7 +75,8 @@ public class JaegerHandler implements RecordingHandler<JaegerContext> {
     } finally {
       child.end();
     }
-    return null;
+*/
+    return tracer;
   }
 
   @Override
